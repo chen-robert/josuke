62e182d51ced32ca8b56d91dbf7194b1dc9a6e49
Robert Chen <robertchen@live.com>
2019-01-09 21:11:36 -0800

    Recursive doc adding

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
 const mammoth = require("mammoth");
+const pdf2html = require('pdf2html')
 
-const testFolder = './docs/';
+const docs = 'docs';
 const fs = require('fs');
 
-var testStr = "";
-fs.readdirSync(testFolder).forEach(file => {
-  if(!file.includes(".docx"))return;
-  console.log(file);
-  mammoth.convertToHtml({path: `${testFolder}${file}`})
-    .then(function(result){
-      var html = result.value; // The generated HTML
-      var messages = result.messages; // Any messages, such as warnings during conversion
-      if(messages)console.log(messages);
-      testStr += html;
-    });
-});
+var htmlSegments = [];
+const explore = folder => {
+  fs.readdirSync(folder).forEach(file => {
+    const fullPath = `${folder}/${file}`;
+    
+    if(fs.lstatSync(fullPath).isDirectory()){
+      explore(fullPath);
+    }else{
+      if(file.endsWith(".docx")){
+        mammoth.convertToHtml({path: fullPath})
+        .then(function(result){
+          var html = result.value; // The generated HTML
+          htmlSegments.push(html);
+          console.log(`Loaded ${fullPath}`);
+        });
+      }else if(file.endsWith(".pdf")){
+        pdf2html.html(`"${fullPath}"`, (err, html) => {
+          if(err)return console.log(fullPath, err);
+          return;
+          htmlSegments.push(html);
+          console.log(`Loaded ${fullPath}`);
+        });
+      }
+    }
+  });
+}
+explore(docs);
 
 
 const express = require("express");
@@ -24,5 +40,5 @@ const PORT = process.env.PORT || 4000;
 const compression = require("compression");
 app.use(compression());
 
-app.get("/", (req, res) => fs.readFile("style.css", "utf8", (err, css) => res.send(`<style>${css}</style>${testStr}`)));
+app.get("/", (req, res) => fs.readFile("style.css", "utf8", (err, css) => res.send(`<style>${css}</style>${htmlSegments.join("")}`)));
 app.listen(PORT, () => console.log(`Started server at port ${PORT}`));
