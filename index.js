a44cc7fd8ce5befe030f6a2f56bad10391e76277
Robert <robertchen@live.com>
2019-02-01 18:43:38 +0000

    Working file upload

--- index.js
+++ index.js
@@ -1,35 +1,8 @@
 const mammoth = require("mammoth");
-const pdf2html = require('pdf2html')
+const pdf2html = require("pdf2html");
 
-const docs = 'docs';
-const fs = require('fs');
-
-var htmlSegments = [];
-const explore = folder => {
-  fs.readdirSync(folder).forEach(file => {
-    const fullPath = `${folder}/${file}`;
-    
-    if(fs.lstatSync(fullPath).isDirectory()){
-      explore(fullPath);
-    }else{
-      if(file.endsWith(".docx")){
-        mammoth.convertToHtml({path: fullPath})
-        .then(function(result){
-          var html = result.value; // The generated HTML
-          htmlSegments.push(html);
-          console.log(`Loaded ${fullPath}`);
-        });
-      }else if(file.endsWith(".pdf")){
-        pdf2html.html(`"${fullPath}"`, (err, html) => {
-          if(err)return console.log(fullPath, err);
-          return;
-          htmlSegments.push(html);
-          console.log(`Loaded ${fullPath}`);
-        });
-      }
-    }
-  });
-}
+const docs = "docs";
+const fs = require("fs");
 
 const express = require("express");
 const cookieParser = require("cookie-parser");
@@ -40,24 +13,64 @@ const PORT = process.env.PORT || 4000;
 
 const compression = require("compression");
 app.use(compression());
-app.use(fileUpload({
-  limits: {
-    fileSize: 50 * 1024 * 1024
-  }
-}));
+app.use(
+  fileUpload({
+    createParentPath: true,
+    abortOnLimit: true,
+    limits: {
+      fileSize: 50 * 1024 * 1024
+    }
+  })
+);
 app.use(cookieParser());
 app.use((req, res, next) => {
-  if(req.cookies.userid === undefined){
+  if (req.cookies.userid === undefined) {
     res.cookie("userid", uuid(), { maxAge: 900000, httpOnly: true });
-  } 
-  next(); 
+  }
+  next();
 });
 
 app.use(express.static("public"));
 
 app.post("/upload", (req, res) => {
-  console.log(req.files.file);
-  console.log(req.cookies.userid);
+  const file = req.files.file;
+  if (file === undefined) return;
+
+  file.mv(
+    `${__dirname}/uploads/${req.cookies.userid}/${file.md5()}_${file.name}`
+  );
+  res.sendStatus(200);
+});
+
+app.get("/data", (req, res) => {
+  var promises = [];
+  var htmlSegments = [];
+  const explore = folder => {
+    fs.readdirSync(folder).forEach(file => {
+      const fullPath = `${folder}/${file}`;
+
+      if (fs.lstatSync(fullPath).isDirectory()) {
+        explore(fullPath);
+      } else {
+        if (file.endsWith(".docx")) {
+          promises.push(
+            mammoth.convertToHtml({ path: fullPath }).then(function(result) {
+              var html = result.value; // The generated HTML
+              htmlSegments.push(html);
+              console.log(`Loaded ${fullPath}`);
+            })
+          );
+        } else if (file.endsWith(".pdf")) {
+          pdf2html.html(`"${fullPath}"`, (err, html) => {
+            if (err) return console.log(fullPath, err);
+            return;
+            htmlSegments.push(html);
+            console.log(`Loaded ${fullPath}`);
+          });
+        }
+      }
+    });
+  };
 });
 
 app.listen(PORT, () => console.log(`Started server at port ${PORT}`));
